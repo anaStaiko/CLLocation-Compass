826fe97ec4a6e28753056d7f9af4f73fe22bc3c9
Anastasiia Staiko <nasskas@gmail.com>
2016-03-07 14:45:21 -0800

    Polylines

--- GPSTrackController.h
+++ GPSTrackController.h
@@ -8,16 +8,22 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
-@interface GPSTrackController : UIViewController <CLLocationManagerDelegate> {
+@interface GPSTrackController : UIViewController <CLLocationManagerDelegate, MKMapViewDelegate, MKOverlay> {
     
     CLLocationManager *lm;
     NSMutableArray *trackPointArray;
+    
+    MKMapRect *routeRect;
+    MKPolylineView *routeLineView;
+    MKPolyline *routeLine;
 }
 
 - (IBAction)startTracking:(id)sender;
-
 - (IBAction)stopTracking:(id)sender;
+- (IBAction)clearTrack:(id)sender;
 
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
--- GPSTrackController.m
+++ GPSTrackController.m
@@ -10,6 +10,7 @@
 
 @implementation GPSTrackController
 
+@synthesize mapView;
 
 -(void)viewDidLoad {
     [super viewDidLoad];
@@ -35,19 +36,11 @@
     lm.desiredAccuracy = kCLLocationAccuracyBest;
     lm.distanceFilter = kCLDistanceFilterNone;
     [lm requestAlwaysAuthorization];
-    //don't forget plist
-    
-    //    [self.locationManager requestAlwaysAuthorization];
     [lm startUpdatingLocation];
-}
-
-- (IBAction)stopTracking:(id)sender {
     
-    lm = [[CLLocationManager alloc] init];
-    [lm stopUpdatingLocation];
-    lm = nil;
+    mapView.delegate = self;
+    mapView.showsUserLocation = self;
 }
-
 
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
     
@@ -57,11 +50,66 @@
     //store latest location in stored track array
     [trackPointArray addObject:currentLocation];
     
-    NSLog(@"%@", trackPointArray);
+    //    NSLog(@"%@", trackPointArray);
+    
+    //get latest location coordinates
+    CLLocationDegrees Latitude = currentLocation.coordinate.latitude;
+    CLLocationDegrees Longitude = currentLocation.coordinate.longitude;
+    CLLocationCoordinate2D locationCoordinates =CLLocationCoordinate2DMake(Latitude, Longitude);
+    
+    //zoom map to show user's location
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(locationCoordinates, 1000, 1000);
+    MKCoordinateRegion adjustRegion = [mapView regionThatFits:viewRegion];
+    [mapView setRegion:adjustRegion animated:YES];
+    
+    NSInteger numberOfSteps = trackPointArray.count;
+    
+    CLLocationCoordinate2D coordinates[numberOfSteps];
+    for (NSInteger index = 0; index < numberOfSteps; index++) {
+        CLLocation *location = [trackPointArray objectAtIndex:index];
+        CLLocationCoordinate2D coordinate2 = location.coordinate;
+        
+        coordinates[index] = coordinate2;
+    }
+    
+    MKPolyline *polyline = [MKPolyline polylineWithCoordinates:coordinates count:numberOfSteps];
+    [mapView addOverlay:polyline];
     
 }
 
 
+-(MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
+    
+    MKPolylineRenderer *polylineRenderer = [[MKPolylineRenderer alloc] initWithOverlay:overlay];
+    polylineRenderer.strokeColor = [UIColor redColor];
+    polylineRenderer.lineWidth = 5.0;
+    
+    return polylineRenderer;
+}
+
+
+- (IBAction)stopTracking:(id)sender {
+    
+    lm = [[CLLocationManager alloc] init];
+    [lm stopUpdatingLocation];
+    lm = nil;
+    
+
+    
+    //stop showing user location
+     mapView.showsUserLocation = NO;
+    
+    //reset array of tracks
+    trackPointArray = nil;
+    trackPointArray = [[NSMutableArray alloc]init];
+    
+}
+
+- (IBAction)clearTrack:(id)sender {
+    //remove overlay on mapView
+    [mapView removeOverlays:mapView.overlays];
+
+}
 
 
 
